706353bddbfb7d4f14a10d379f4cf065b3616e1a
Kaiser <kaiserzulkarnain@gmail.com>
2024-04-02 09:12:11 +0700

    feat: Header mobile breakpoint

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,20 +6,25 @@ import { ArrowWhiteProp } from "./svgs";
 const Header = () => {
   return (
     <div className="w-full h-auto max-auto">
-      <div className="w-full h-[650px] max-w-[1440px] mx-auto flex items-center">
-        <div className="w-full lg:min-w-[610px] md:min-w-[495px] h-full flex items-center justify-center bg-black">
+      <div className="w-full h-[650px] max-w-[1440px] mx-auto flex md:flex-row flex-col items-center">
+        <div className="w-full lg:min-w-[610px] md:min-w-[495px] h-full md:p-0 px-[33px] py-20 flex items-center justify-center bg-black">
           <div className="flex flex-col items-start gap-y-5">
-            <div className="text-[40px] leading-[48px] text-white font-bold tracking-widest">
+            <div className="md:text-[40px] text-[32px] md:leading-[48px] leading-[40px] text-white font-bold tracking-widest">
               CREATE AND
               <br />
               SHARE YOUR
               <br />
               PHOTO STORIES.
             </div>
-            <div className="text-zinc-400">
-              Photosnap is a platform for photographers and visual
-              <br /> storytellers. We make it easy to share photos, tell
-              <br /> stories and connect with others.
+            <div className="md:text-md md:leading-normal text-[15px] leading-[25px] text-zinc-400">
+              Photosnap is a platform for photographers
+              <br className="block md:hidden" /> and visual
+              <br className="md:block hidden" /> storytellers. We make it easy
+              to
+              <br className="block md:hidden" /> share photos, tell
+              <br className="md:block hidden" /> stories and connect with
+              <br className="block md:hidden" />
+              others.
             </div>
             <Link
               href="#"
@@ -30,7 +35,7 @@ const Header = () => {
             </Link>
           </div>
         </div>
-        <div className="w-full max-w-[830px] h-[650px] bg-createShare bg-cover bg-center" />
+        <div className="w-full max-w-[830px] md:min-h-[650px] min-h-[294px] bg-createShare bg-cover bg-center md:order-last order-first" />
       </div>
     </div>
   );
