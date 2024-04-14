ce44a3404a8ee816c1991b6b0f52f413989e3c13
Kaiser <kaiserzulkarnain@gmail.com>
2024-04-14 13:43:57 +0700

    feat: switch

--- src/components/PricingPrice.tsx
+++ src/components/PricingPrice.tsx
@@ -1,15 +1,32 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import { Switch } from "@/components/ui/switch";
+
 const PricingPrice = () => {
+  const [isYearly, setIsYearly] = useState(true);
+  const handlePriceChange = () => {
+    setIsYearly(!isYearly);
+  };
   return (
     <div className="w-full h-auto">
       <div className="w-full h-auto px-[165px] py-28 max-w-[1440px] mx-auto bg-white flex flex-col items-center gap-y-20">
         <div className="flex flex-col items-center">
           <div className="w-[255px] h-auto flex items-center justify-around">
-            <div>Monthly</div>
-            <Switch className="scale-[1.45]" />
-            <div>Yearly</div>
+            <div
+              className={`text-lg font-bold text-black ${
+                isYearly ? "opacity-100" : "opacity-50"
+              }`}
+            >
+              Monthly
+            </div>
+            <Switch onClick={handlePriceChange} />
+            <div
+              className={`text-lg font-bold text-black ${
+                isYearly ? "opacity-50" : "opacity-100"
+              }`}
+            >
+              Yearly
+            </div>
           </div>
         </div>
       </div>
