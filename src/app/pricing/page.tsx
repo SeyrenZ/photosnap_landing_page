5e962b1e91dca1f4f40bc675553edaf13e618695
Kaiser <kaiserzulkarnain@gmail.com>
2024-04-15 12:54:04 +0700

    feat: price card ui

--- src/app/pricing/page.tsx
+++ src/app/pricing/page.tsx
@@ -1,8 +1,14 @@
+"use client";
 import { Banner, PricingPrice } from "@/components";
 import PricingHeader from "@/components/PricingHeader";
 import React from "react";
-
+import { useEffect } from "react";
+import Aos from "aos";
+import "aos/dist/aos.css";
 const Home = () => {
+  useEffect(() => {
+    Aos.init();
+  }, []);
   return (
     <div className="pt-[72px]">
       <PricingHeader />
--- src/components/PricingPrice.tsx
+++ src/components/PricingPrice.tsx
@@ -9,8 +9,8 @@ const PricingPrice = () => {
   };
   return (
     <div className="w-full h-auto">
-      <div className="w-full h-auto px-[165px] py-28 max-w-[1440px] mx-auto bg-white flex flex-col items-center gap-y-20">
-        <div className="flex flex-col items-center">
+      <div className="w-full h-auto py-28 max-w-[1440px] mx-auto bg-white flex flex-col items-center gap-y-20">
+        <div className="flex flex-col items-center" data-aos="fade-down">
           <div className="w-[255px] h-auto flex items-center justify-around">
             <div
               className={`text-lg font-bold text-black ${
@@ -29,64 +29,76 @@ const PricingPrice = () => {
             </div>
           </div>
         </div>
-        <div className="flex items-center gap-x-[30px]">
-          <div className="w-[350px] h-[407px] py-12 px-9 bg-zinc-100 transition ease-in-out duration-300 flex flex-col items-center justify-between">
-            <div className="flex flex-col items-center gap-y-[18px]">
+        <div
+          className="w-full sm:px-10 flex lg:flex-row flex-col items-center justify-center gap-x-[30px] lg:gap-y-0 gap-y-6"
+          data-aos="zoom-out"
+        >
+          <div className="w-full lg:max-w-[350px] sm:max-w-[689px] max-w-[320px] lg:h-[407px] sm:h-[270px] h-[407px] lg:py-12 lg:px-9 sm:p-10 pt-14 pb-10 px-9 bg-zinc-100 transition ease-in-out duration-300 flex lg:flex-col sm:flex-row flex-col lg:items-center sm:items-start items-center justify-between">
+            <div className="lg:w-auto w-full max-w-[270px] flex flex-col lg:items-center sm:items-start items-center gap-y-[18px]">
               <div className="text-2xl font-bold text-black">Basic</div>
-              <div className="text-[15px] leading-[25px] font-medium text-zinc-500 text-center">
+              <div className="text-[15px] leading-[25px] font-medium text-zinc-500 lg:text-center sm:text-left text-center">
                 Includes basic usage of our platform. Recommended for new and
                 aspiring photographers.
               </div>
+              <button className="w-full h-fit bg-black hover:bg-zinc-300 py-3 text-xs text-white hover:text-black font-bold tracking-widest transition ease-in-out duration-300 sm:block lg:hidden hidden">
+                PICK PLAN
+              </button>
             </div>
             <div className="flex flex-col items-center">
               <div className="text-[40px] leading-[48px] font-bold text-black">
-                $19.00
+                {isYearly ? "$19.00" : "$190.00"}
               </div>
               <div className="text-[15px] leading-[25px] font-medium text-zinc-500">
-                per month
+                {isYearly ? "per month" : "per year"}
               </div>
             </div>
-            <button className="w-full h-fit bg-black hover:bg-zinc-300 py-3 text-xs text-white hover:text-black font-bold tracking-widest transition ease-in-out duration-300">
+            <button className="w-full h-fit bg-black hover:bg-zinc-300 py-3 text-xs text-white hover:text-black font-bold tracking-widest transition ease-in-out duration-300 lg:block sm:hidden">
               PICK PLAN
             </button>
           </div>
-          <div className="w-[350px] h-[470px] py-[80px] px-9 bg-black transition ease-in-out duration-300 flex flex-col items-center justify-between">
-            <div className="flex flex-col items-center gap-y-[18px]">
+          <div className="w-full lg:max-w-[350px] sm:max-w-[689px] max-w-[320px] lg:h-[470px] sm:h-[270px] h-[407px] lg:py-12 lg:px-9 sm:p-10 pt-14 pb-10 px-9 bg-black transition ease-in-out duration-300 flex lg:flex-col sm:flex-row flex-col lg:items-center sm:items-start items-center justify-between">
+            <div className="lg:w-auto w-full max-w-[270px] flex flex-col lg:items-center sm:items-start items-center gap-y-[18px]">
               <div className="text-2xl font-bold text-white">Pro</div>
-              <div className="text-[15px] leading-[25px] font-medium text-zinc-500 text-center">
+              <div className="text-[15px] leading-[25px] font-medium text-zinc-500 lg:text-center sm:text-left text-center">
                 More advanced features available. Recommended for photography
                 veterans and professionals.
               </div>
+              <button className="w-full h-fit bg-white hover:bg-zinc-300 py-3 text-xs text-black font-bold tracking-widest transition ease-in-out duration-300 sm:block lg:hidden hidden">
+                PICK PLAN
+              </button>
             </div>
             <div className="flex flex-col items-center">
               <div className="text-[40px] leading-[48px] font-bold text-white">
-                $39.00
+                {isYearly ? "$39.00" : "$390.00"}
               </div>
               <div className="text-[15px] leading-[25px] font-medium text-zinc-500">
-                per month
+                {isYearly ? "per month" : "per year"}
               </div>
             </div>
-            <button className="w-full h-fit bg-white hover:bg-zinc-300 py-3 text-xs text-black font-bold tracking-widest transition ease-in-out duration-300">
+            <button className="w-full h-fit bg-white hover:bg-zinc-300 py-3 text-xs text-black font-bold tracking-widest transition ease-in-out duration-300 lg:block sm:hidden">
               PICK PLAN
             </button>
           </div>
-          <div className="w-[350px] h-[407px] py-12 px-9 bg-zinc-100 transition ease-in-out duration-300 flex flex-col items-center justify-between">
-            <div className="flex flex-col items-center gap-y-[18px]">
+          <div className="w-full lg:max-w-[350px] sm:max-w-[689px] max-w-[320px] lg:h-[407px] sm:h-[270px] h-[407px] lg:py-12 lg:px-9 sm:p-10 pt-14 pb-10 px-9 bg-zinc-100 transition ease-in-out duration-300 flex lg:flex-col sm:flex-row flex-col lg:items-center sm:items-start items-center justify-between">
+            <div className="lg:w-auto w-full max-w-[270px] flex flex-col lg:items-center sm:items-start items-center gap-y-[18px]">
               <div className="text-2xl font-bold text-black">Business</div>
-              <div className="text-[15px] leading-[25px] font-medium text-zinc-500 text-center">
+              <div className="text-[15px] leading-[25px] font-medium text-zinc-500 lg:text-center sm:text-left text-center">
                 Additional features available such as more detailed metrics.
                 Recommended for business owners.
               </div>
+              <button className="w-full h-fit bg-black hover:bg-zinc-300 py-3 text-xs text-white hover:text-black font-bold tracking-widest transition ease-in-out duration-300 sm:block lg:hidden hidden">
+                PICK PLAN
+              </button>
             </div>
             <div className="flex flex-col items-center">
               <div className="text-[40px] leading-[48px] font-bold text-black">
-                $99.00
+                {isYearly ? "$99.00" : "$990.00"}
               </div>
               <div className="text-[15px] leading-[25px] font-medium text-zinc-500">
-                per month
+                {isYearly ? "per month" : "per year"}
               </div>
             </div>
-            <button className="w-full h-fit bg-black hover:bg-zinc-300 py-3 text-xs text-white hover:text-black font-bold tracking-widest transition ease-in-out duration-300">
+            <button className="w-full h-fit bg-black hover:bg-zinc-300 py-3 text-xs text-white hover:text-black font-bold tracking-widest transition ease-in-out duration-300 lg:block sm:hidden">
               PICK PLAN
             </button>
           </div>
