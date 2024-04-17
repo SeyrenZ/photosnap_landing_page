59b057f59f2faac1d9cf24d944c7fffdf7b32a6b
Kaiser <kaiserzulkarnain@gmail.com>
2024-04-17 11:42:38 +0700

    feat: update

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -5,7 +5,9 @@ import { ArrowWhiteProp } from "./svgs";
 const Banner = () => {
   return (
     <div className="w-full h-auto">
-      <div className="w-full h-[280px] max-w-[1440px] mx-auto lg:px-[80px] sm:px-[39px] px-[33px] bg-BannerBackground bg-cover bg-no-repeat flex items-center">
+      <div className="w-full h-[280px] max-w-[1440px] mx-auto lg:px-[80px] sm:px-[39px] px-[33px] bg-BannerBackground bg-cover bg-no-repeat flex items-center relative">
+        <div className="absolute w-1.5 h-full bg-white left-0 bg-gradient-to-t from-[#FFC593] via-[#BC7198] to-[#5A77FF] sm:block hidden" />
+        <div className="absolute w-32 h-1.5 bg-white top-0 left-8 bg-gradient-to-r from-[#FFC593] via-[#BC7198] to-[#5A77FF] block sm:hidden" />
         <div className="w-full h-full flex sm:flex-row flex-col sm:items-center sm:justify-between items-start justify-center sm:gap-y-0 gap-y-6">
           <div
             className="w-full max-w-[400px] sm:text-[40px] sm:leading-[48px] text-[32px] leading-[40px] font-bold text-white tracking-widest uppercase"
--- src/components/features/FeaturesHeader.tsx
+++ src/components/features/FeaturesHeader.tsx
@@ -7,7 +7,9 @@ const FeaturesHeader = () => {
   return (
     <div className="w-full md:h-[490px] h-auto">
       <div className="w-full h-full max-w-[1440px] mx-auto flex md:flex-row flex-col items-center">
-        <div className="w-full lg:max-w-[610px] md:min-w-[495px] h-full md:p-0 px-[33px] py-20 flex items-center justify-center bg-black">
+        <div className="w-full lg:max-w-[610px] md:min-w-[495px] h-full md:p-0 px-[33px] py-20 flex items-center justify-center bg-black relative">
+          <div className="absolute w-1.5 h-[144px] bg-white left-0 bg-gradient-to-t from-[#FFC593] via-[#BC7198] to-[#5A77FF] sm:block hidden" />
+          <div className="absolute w-32 h-1.5 bg-white top-0 left-8 bg-gradient-to-r from-[#FFC593] via-[#BC7198] to-[#5A77FF] block sm:hidden" />
           <div
             className="flex flex-col items-start gap-y-5"
             data-aos="fade-right"
